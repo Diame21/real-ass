1843cf562306980303e6e404af0ee91502415ce4
Diame21 <diamevictory@gmail.com>
2025-09-07 23:46:18 +0100

    updated file

---
+++ java_ass4.js
@@ -0,0 +1,39 @@
+// console.log("30 days challenge")
+let challenge = '30 days of Javascript'
+
+console.log(challenge)
+
+// length of the String
+console.log(challenge.length)
+// OR 
+let check = challenge.length
+
+// TO UPPERCASE 
+let upper =challenge.toUpperCase()
+console.log(upper)
+// TO LOWERCASE 
+let lower =challenge.toLowerCase()
+console.log(lower)
+// TO CUT OUT THR FIRST OF THE SUBSTRING 
+let cut =challenge.substring(0, 2)
+console.log(cut)
+//SLICE OUT DAYS OF JAVASCRIPT FROM PHRASE
+let slice =challenge.substring(3)
+console.log(slice)
+// TO CHECK IF STRING CONTAINS SCRIPT 
+let change = challenge.includes("script")
+console.log(change)
+// TO SPLIT THE SCRIPT AT THE SPACE
+let divide = challenge.split(' ')
+console.log(divide)
+// TO SPLIT THE TECH COMPANIES STRING INTO ARRAY
+let companies = 'Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon'
+console.log(companies.split(','))
+
+
+
+
+
+
+
+
