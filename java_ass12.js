0e4895c0c84097cecf9399d11b3d6c36ecc8cf25
Diame21 <diamevictory@gmail.com>
2025-10-02 02:04:18 +0100

    added new file

---
+++ java_ass12.js
@@ -0,0 +1,10 @@
+let i = 100                  
+for (let i = 0; i <= 200; i++) {
+    if (i % 2 === 0) {
+        console.log(`${i} is a even number`);
+    }
+    else{
+        console.log(`${i} is an odd number`);
+    }
+    
+}
